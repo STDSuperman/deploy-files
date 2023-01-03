06d56d1a667be828418935f0428c81736610841a
xiaohang.lin <xiaohang.lin@alibaba-inc.com>
2023-01-03 22:58:47 +0800

    feat: first init

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+async function run(): Promise<void> {
+  try {
+    const ms: string = core.getInput('milliseconds')
+    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+    core.debug(new Date().toTimeString())
+
+    core.debug('Will print file tree')
+
+    exec.exec('ls');
+
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
